faadce5c4f693f3f6baae010dc2d7ec995b18600
lama <aloatibilama196.com>
2025-05-11 15:15:46 +0300

    23

--- navbar.js
+++ navbar.js
@@ -1,11 +1,14 @@
 export const navbar = `
+
 <header>
+
   <h2><a href="/Games2/index.html" class="heder">Game</a></h2>
   <ul>
     <li><a href="/Games2/Horror/gameshorror.html">Horror</a></li>
     <li><a href="/Games2/Adventure/gamesadventure.html">Adventure</a></li>
     <li><a href="/Games2/contact/contact1.html">Contact</a></li>
   </ul>
-  
+ 
 </header>
+
 `;
