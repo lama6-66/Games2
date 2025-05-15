698bafea5232660bd641e688b88655fee2d15f52
lama <aloatibilama196.com>
2025-05-15 14:40:09 +0300

    31

--- load-navbar.js
+++ load-navbar.js
@@ -1,2 +1,11 @@
 import { navbar } from './navbar.js';
 document.getElementById("navbar-placeholder").innerHTML = navbar;
+
+// document.getElementById("navbar-placeholder").innerHTML = navbar;
+
+window.toggleMenu = function () {
+  const menu = document.getElementById('navbar-links');
+  if (menu) {
+    menu.classList.toggle('active');
+  }
+};
--- navbar.js
+++ navbar.js
@@ -2,14 +2,35 @@ export const navbar = `
 
 <header>
 
+
+
+
   <h2><a href="/Games2/index.html" class="heder">Game</a></h2>
-  <ul>
+  
+  <div class="menu-toggle" onclick="toggleMenu()">☰</div>
+  <ul id="navbar-links">
     <li><a href="/Games2/Horror/gameshorror.html">Horror</a></li>
     <li><a href="/Games2/Adventure/gamesadventure.html">Adventure</a></li>
+    <li><a href="/Games2/contact/play.html">play</a></li>
     <li><a href="/Games2/contact/contact1.html">Contact</a></li>
+
+    
     
   </ul>
  
 </header>
 
+
+
 `;
+
+
+
+function toggleMenu() {
+  const menu = document.getElementById('navbar-links');
+  if (menu) {
+    menu.classList.toggle('active');
+  }
+}
+
+  
